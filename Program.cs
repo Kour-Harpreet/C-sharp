27ea331377daa24ddc386775d3a4159504add3be
harpreet <Harpreet Kour>
2020-06-24 14:12:07 -0600

    Updated hello world text

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace C_sharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, TECHCareers !");
         }
     }
 }
