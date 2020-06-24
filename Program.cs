19f6dacdedf3c86429521d46b8c56bbd21657b0a
harpreet <Harpreet Kour>
2020-06-24 15:41:16 -0600

    Add retrieve user input; add string interpolation.

--- Program.cs
+++ Program.cs
@@ -16,6 +16,16 @@ namespace C_sharp
 
             ....and we end multi-line comments with asterisk followed by a forward slash */
             Console.WriteLine("Hello, " + myName);
+
+            Console.WriteLine("Please enter your name..");
+
+            // Assign variable 'username' to a value entered by the user.
+            string userName = Console.ReadLine();
+            // show the user we colect the name.
+            Console.WriteLine("you have entered: "+userName);
+            //Instead of traditional concatenation using the addition
+            //We can use string interpolation (placeholders) to embed values in a string.
+            Console.WriteLine("Hey {0}, you're looking great today!", userName);
         }
     }
 }
