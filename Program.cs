0369fe235667cd4765a3a102d83a1ce9926cee5a
harpreet <Harpreet Kour>
2020-06-24 15:13:52 -0600

    Add an assignment; output concatenated hello string.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace C_sharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, TECHCareers !");
+            // two slashes is to begin single line comment
+            // we are assigning string value to a variable
+            string myName="Harpreet Kour";
+            /* we begin multi line comment with forward slash and asterisk
+
+
+            + symbol which we are using is to concatenate two strings together.
+
+            ....and we end multi-line comments with asterisk followed by a forward slash */
+            Console.WriteLine("Hello, " + myName);
         }
     }
 }
