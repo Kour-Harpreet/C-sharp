5880ca09d01b997cde489616334b3458ab636f41
harpreet <Harpreet Kour>
2020-06-25 10:36:30 -0600

    Added datatypes examples; added mathematical operators.

--- Program.cs
+++ Program.cs
@@ -5,36 +5,44 @@ namespace C_sharp
     class Program
     {
         static void Main(string[] args)
-        {
-            // two slashes is to begin single line comment
-            // we are assigning string value to a variable
-            string myName="Harpreet Kour";
-            /* we begin multi line comment with forward slash and asterisk
+        {  
+            // "=" is the assignment operator!
+            //String datatype is used for text.
+            string myString = " This is a string!";
+            // Integer datatype is used for whole numbers (no decimals.)
+            int myInt = 42;
+            // double datatypes is used for decimal point numbers.
+            double myDouble = 33.666;
+            // Boolean datatype is used for TRUE/FALSE values - think binary!
+            bool myBool = true;
+
+            //Subtraction operator is "-"(minus.)
+            Console.WriteLine( myInt - 10 ); //32
 
 
-            + symbol which we are using is to concatenate two strings together.
+            //Multiplication operator is "*" (multiply)
+            Console.WriteLine( myInt * 2); // 84
 
-            ....and we end multi-line comments with asterisk followed by a forward slash */
-            Console.WriteLine("Hello, " + myName);
 
-            Console.WriteLine("Please enter your name..");
+            //Division operator "/" (division)
+            Console.WriteLine( myInt / 2); // 21
 
-            // Assign variable 'username' to a value entered by the user.
-            string userName = Console.ReadLine();
-            // show the user we colect the name.
-            Console.WriteLine("you have entered: "+userName);
-            //Instead of traditional concatenation using the addition
-            //We can use string interpolation (placeholders) to embed values in a string.
-            //https://docs.microsoft.com/en-us/dotnet/api/system.console.readline?view=netcore-3.1
+            //Modulus operator is % (percentage)
+            Console.WriteLine( myInt % 13); //3
 
-            Console.WriteLine("Hey {0}, you're looking great today!", userName);
+            //Addition operator is " + " (plus)
+            //Wait ! this is also the concatenation operator.
+            Console.WriteLine( myInt + 8); // 50
 
-            //let's play with string!
-            Console.WriteLine( "makemeuppercase".ToUpper() );
-            Console.WriteLine( "MAKEMELOWERCASE".ToLower() );
-            // substring takes two arguments: 1) starting character position 2) Number of character to collect.
-            Console.WriteLine( "ABCDEFGHIJKLMNOPQRSTUVWXYZ".Substring(3,3));
-            Console.WriteLine( "XYZ".Length); // get the number of character for the string.
+
+
+            Console.WriteLine("3" + "3"); // 33
+            Console.WriteLine("3" + 3); // 33
+            Console.WriteLine( 3 + "3"); // 33
+            Console.WriteLine( 3 + 3); // 6
+            Console.WriteLine( 9 + 3 + "3"); // 93
+            
+
 
 
 
