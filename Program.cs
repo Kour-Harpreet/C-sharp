db491767adf7517d174c69336c9f7013342dd5ed
harpreet <Harpreet Kour>
2020-06-25 15:16:37 -0600

    Added example of loops (while, for and foreach); add example of list

--- Program.cs
+++ Program.cs
@@ -6,27 +6,37 @@ namespace C_sharp
     {
         static void Main(string[] args)
         { 
-            Console.WriteLine( "enter a number:" );
-            int userInt = Int32.Parse(Console.ReadLine() );
-            if ( userInt > 5 )
+            int myNum = 3;
+            while ( myNum > 0  ) // Iteration/loops while the CONDITION is true.
             {
-                Console.WriteLine( " Greater than 5 but less than 11.");
-            
-            }
-            else if ( userInt > 10 )
-            {
-                Console.WriteLine( " Greater than 10.");
-            
-            }
-            else if ( userInt < 0 )
-            {
-                Console.WriteLine( " A negative number.");
-            
-            }
-           else{
-               Console.WriteLine(" Number is 1 and 4 ");
-           }
 
+                Console.WriteLine( " Current number is {0}", myNum );
+                myNum--;
+            }
+
+            /* For loops have 3 semi-colon-separated components:
+            1) Assignment.
+            2) [Termination] Condition.
+            3) Iteration 
+            */
+
+            for( int i = 0; i < 6; i += 2 )
+            {
+                Console.WriteLine("Current (for) number is :{0}", i);
+
+            }
+
+            // If we need to iterate a list, use foreach!
+
+            string[] myList = { "cat", "turtle", "lizard", "frog", "dog"};
+            foreach ( string listItem in myList)
+            {
+                Console.WriteLine(" the current item is : {0}", listItem);
+            }
+
+
+
+            
           
 
         }
