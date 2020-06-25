b7197386c24a0b874dc55bcdcad0853fd6853152
harpreet <Harpreet Kour>
2020-06-25 16:00:08 -0600

    add while loop + switch statement example

--- Program.cs
+++ Program.cs
@@ -5,41 +5,60 @@ namespace C_sharp
     class Program
     {
         static void Main(string[] args)
-        { 
-            int myNum = 3;
-            while ( myNum > 0  ) // Iteration/loops while the CONDITION is true.
+        {
+            //guide the user
+            Console.WriteLine("please enter add/subtract/multiply/exit:");
+
+            //collect the user input....
+            string userInput;
+            while ((userInput = Console.ReadLine()) != "exit") // LOOPS until user enters "exit" command.
             {
+                int firstNum;
+                int secondNum;
+                int result;
+                //Switch statements are similar to if/else.
+                switch (userInput) // Here we enter the value we want to test.
+                {// Cases are us testing the above against specific values.
+                    case "add":
+                        Console.WriteLine("Enter first number for addition:");
+                        firstNum = Int32.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter second number for addition:");
+                        secondNum = Int32.Parse(Console.ReadLine());
+                        result = firstNum + secondNum;
+                        Console.WriteLine("{0} + {1} = {2}", firstNum, secondNum, result);
 
-                Console.WriteLine( " Current number is {0}", myNum );
-                myNum--;
-            }
+                        break;
+                    case "subtract":
+                        Console.WriteLine("Enter first number for subtraction:");
+                        firstNum = Int32.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter second number for subtraction:");
+                        secondNum = Int32.Parse(Console.ReadLine());
+                        result = firstNum - secondNum;
+                        Console.WriteLine("{0} - {1} = {2}", firstNum, secondNum, result);
+                        break;
+                    case "multiply":
+                        Console.WriteLine("Enter first number for multiplication:");
+                        firstNum = Int32.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter second number for multiplication:");
+                        secondNum = Int32.Parse(Console.ReadLine());
+                        result = firstNum * secondNum;
+                        Console.WriteLine("{0} * {1} = {2}", firstNum, secondNum, result);
 
-            /* For loops have 3 semi-colon-separated components:
-            1) Assignment.
-            2) [Termination] Condition.
-            3) Iteration 
-            */
-
-            for( int i = 0; i < 6; i += 2 )
-            {
-                Console.WriteLine("Current (for) number is :{0}", i);
-
-            }
-
-            // If we need to iterate a list, use foreach!
-
-            string[] myList = { "cat", "turtle", "lizard", "frog", "dog"};
-            foreach ( string listItem in myList)
-            {
-                Console.WriteLine(" the current item is : {0}", listItem);
-            }
-
-
-
-            
-          
-
+                        break;
+                    default:
+                        Console.WriteLine("Unrecognized operator/command entered.");
+                        break;
+                } // End of switch statement.
+                Console.WriteLine("Please enter a new command: add/subtract/multiply/exit.");
+            } // End of while statement.
         }
 
     }
+
 }
+
+
+
+
+
+
