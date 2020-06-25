f7b345d5b89b175f8ba182d857dd51404e69de2b
harpreet <Harpreet Kour>
2020-06-25 14:06:29 -0600

    Addes subtraction (else if) and multiply calculation

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace C_sharp
     {
         static void Main(string[] args)
         { 
-            Console.WriteLine( " Please tell us if you want to add or subtract: (add/subtract)");
+            // Always tell user what to do
+            Console.WriteLine( " Please tell us if you want to add or subtract: (add/subtract/multiply)");
             string userOperator = Console.ReadLine();
 
             if( userOperator == "add")
@@ -24,6 +25,38 @@ namespace C_sharp
                 Console.WriteLine( "{0} + {1} = {2}", firstNum, secondNum, result );
 
             }
+            else if ( userOperator == "subtract" )
+            {
+                Console.WriteLine(" Please enter the first number to subtract: ");
+                
+                //https://stackoverflow.com/questions/1019793/how-can-i-convert-string-to-int
+               
+                int firstNum = Int32.Parse( Console.ReadLine()); // Convert to integer.
+                
+                Console.WriteLine(" Please enter the second number to subtract: ");
+                int secondNum = Int32.Parse( Console.ReadLine());
+
+                int result = firstNum - secondNum;
+                Console.WriteLine( "{0} - {1} = {2}", firstNum, secondNum, result );
+            }
+            else if ( userOperator == "multiply" )
+            {
+                Console.WriteLine(" Please enter the first number to multiply: ");
+                
+                //https://stackoverflow.com/questions/1019793/how-can-i-convert-string-to-int
+               
+                int firstNum = Int32.Parse( Console.ReadLine()); // Convert to integer.
+                
+                Console.WriteLine(" Please enter the second number to multiply: ");
+                int secondNum = Int32.Parse( Console.ReadLine());
+
+                int result = firstNum * secondNum;
+                Console.WriteLine( "{0} * {1} = {2}", firstNum, secondNum, result );
+            }
+             else
+             {
+                 Console.WriteLine( " Invalid operator entered." );
+             }
           
 
         }
